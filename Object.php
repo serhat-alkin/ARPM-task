f45741a5e73994412febb943e0007a3212a1d684
serhat-alkin <alkinserhat@gmail.com>
2023-03-15 14:55:07 +0300

    update object file

--- Object.php
+++ Object.php
@@ -1,20 +1,19 @@
 ﻿<?php
 class Foo
 {
-	/** @var string **/
-	private $sHelloString = 'Hello world!';
-	
-	/** @var array **/
-	public $aArray = [];
+  /** @var string **/
+  private $sHelloString = 'Hello world!';
+  
+  /** @var array **/
+  public $aArray = [];
 
-	public function printString(): void
-	{
-			echo $this->helloString;
-	}
-
-	/** Write here the method */
-
-	public function __toString(): string
+  public function printString(): void
+  {
+      echo $this->helloString;
+  }
+    
+  /** Write here the method */
+  public function __toString(): string
   {
     return json_encode(get_object_vars($this));
   }
